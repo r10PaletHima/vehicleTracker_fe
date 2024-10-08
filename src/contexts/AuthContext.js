3d8a9923b5df5c79a2c7f3a93b93fe8ed2b75dc4
PAVAN <pavan@7dliving.com>
2024-10-08 15:43:42 +0530

    minor

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,6 +7,8 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  
+
   useEffect(() => {
     const initAuth = async () => {
       const token = localStorage.getItem("authToken");
